33d8f650981451351fcb1de4d7f7d1e8f9acce1f
= <devhenriquerodrigues971@gmail.com>
2026-07-06 18:01:37 -0300

    fix(address): corrigir nomenclatura de coluna.

--- src/main/resources/db/migration/V6__create_address_table.sql
+++ src/main/resources/db/migration/V6__create_address_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS address(
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     street VARCHAR(200) NOT NULL,
-    addressNumber INT,
+    address_number INT,
     complement VARCHAR(200),
     neighborhood_id UUID NOT NULL,
     CONSTRAINT fk_address_neighborhood FOREIGN KEY (neighborhood_id) REFERENCES neighborhood(id)
