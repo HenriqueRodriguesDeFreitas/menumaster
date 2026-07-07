8f753dfe8fd15a98310ca2918761f05c54dbcc6a
= <devhenriquerodrigues971@gmail.com>
2026-07-07 13:44:34 -0300

    fix(address): add relacionamento com neighborhood.

--- src/main/java/com/paulo/menumaster/model/Address.java
+++ src/main/java/com/paulo/menumaster/model/Address.java
@@ -15,6 +15,47 @@ public class Address {
     @Column(length = 200, nullable = false)
     private String street;
 
-    @Column
+    @Column(name = "address_number")
     private int addressNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "neighborhood_id", foreignKey = @ForeignKey(name = "fk_address_neighborhood"))
+    private Neighborhood neighborhood;
+
+    public Address() {
+    }
+
+    public Address(String street, int addressNumber, Neighborhood neighborhood) {
+        this.street = street;
+        this.addressNumber = addressNumber;
+        this.neighborhood = neighborhood;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(int addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public Neighborhood getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(Neighborhood neighborhood) {
+        this.neighborhood = neighborhood;
+    }
 }
