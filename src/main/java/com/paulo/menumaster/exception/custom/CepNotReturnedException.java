04cd661011082d5724ca4df583534e39ff55105e
= <devhenriquerodrigues971@gmail.com>
2026-07-08 14:43:00 -0300

    fix(exception): criar classe para cep nao retornado.

---
+++ src/main/java/com/paulo/menumaster/exception/custom/CepNotReturnedException.java
@@ -0,0 +1,7 @@
+package com.paulo.menumaster.exception.custom;
+
+public class CepNotReturnedException extends RuntimeException {
+    public CepNotReturnedException(String message) {
+        super(message);
+    }
+}
