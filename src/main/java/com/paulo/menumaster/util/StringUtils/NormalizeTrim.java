8e919a2b24ede6999bef58c447ef934dccd3f0bd
= <devhenriquerodrigues971@gmail.com>
2026-07-09 20:18:19 -0300

    feat(normalize): criar metodo util para normalizar textos.

---
+++ src/main/java/com/paulo/menumaster/util/StringUtils/NormalizeTrim.java
@@ -0,0 +1,10 @@
+package com.paulo.menumaster.util.StringUtils;
+
+public class NormalizeTrim {
+    private NormalizeTrim() {
+    }
+
+    public static String normalizeTrim(String text) {
+        return text.trim();
+    }
+}
