4a18b9b03bd8cbbeaf0d931ad6d3ef7f8df7adaf
= <devhenriquerodrigues971@gmail.com>
2026-07-09 20:18:57 -0300

    fix(state): add repositori ode busca por nome.

--- src/main/java/com/paulo/menumaster/repository/StateRepository.java
+++ src/main/java/com/paulo/menumaster/repository/StateRepository.java
@@ -2,8 +2,12 @@ package com.paulo.menumaster.repository;
 
 import com.paulo.menumaster.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StateRepository extends JpaRepository<State, UUID> {
+    @Query("SELECT s FROM State s WHERE function('unaccent', lower(s.name)) = function('unaccent', lower(:name))")
+    Optional<State> findByNameIgnoreCaseUnaccent(String name);
 }
