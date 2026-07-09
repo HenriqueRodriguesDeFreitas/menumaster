7da9133ba393108d3a108d8f987d8b0be5bc7d47
= <devhenriquerodrigues971@gmail.com>
2026-07-09 19:40:03 -0300

    fix(customer): repository no service.

--- src/main/java/com/paulo/menumaster/service/CustomerService.java
+++ src/main/java/com/paulo/menumaster/service/CustomerService.java
@@ -1,7 +1,26 @@
 package com.paulo.menumaster.service;
 
+import com.paulo.menumaster.integration.ViaCepIntegration;
+import com.paulo.menumaster.repository.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final AddressRepository addressRepository;
+    private final CityRepository cityRepository;
+    private final NeighborhoodRepository neighborhoodRepository;
+    private final StateRepository stateRepository;
+    private final ViaCepIntegration viaCepIntegration;
+
+    public CustomerService(CustomerRepository customerRepository, AddressRepository addressRepository,
+                           CityRepository cityRepository, NeighborhoodRepository neighborhoodRepository,
+                           StateRepository stateRepository, ViaCepIntegration viaCepIntegration) {
+        this.customerRepository = customerRepository;
+        this.addressRepository = addressRepository;
+        this.cityRepository = cityRepository;
+        this.neighborhoodRepository = neighborhoodRepository;
+        this.stateRepository = stateRepository;
+        this.viaCepIntegration = viaCepIntegration;
+    }
 }
