6ef91ca9315144d6c942064aeaf7721ebd38c8dc
= <devhenriquerodrigues971@gmail.com>
2026-07-11 12:06:43 -0300

    fix(city): add metodo de verificação se cidade existe.

--- src/main/java/com/paulo/menumaster/repository/CityRepository.java
+++ src/main/java/com/paulo/menumaster/repository/CityRepository.java
@@ -1,9 +1,16 @@
 package com.paulo.menumaster.repository;
 
 import com.paulo.menumaster.model.City;
+import com.paulo.menumaster.model.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface CityRepository extends JpaRepository<City, UUID> {
+    @Query("""
+        SELECT c FROM City c WHERE  FUNCTION('unaccent', LOWER(c.name)) = FUNCTION('unaccent', LOWER(:name)) AND c.state = :state
+        """)
+    boolean existsByNameignoreCaseAndState(@Param("name") String name, @Param("state") State state);
 }
