336aa556fe9886dc0a0c44b29337c273e7d1c47b
= <devhenriquerodrigues971@gmail.com>
2026-07-11 12:07:18 -0300

    fix(customer): add metodo pra verificar se estado possui cidade.

--- src/main/java/com/paulo/menumaster/service/CustomerService.java
+++ src/main/java/com/paulo/menumaster/service/CustomerService.java
@@ -2,9 +2,13 @@ package com.paulo.menumaster.service;
 
 import com.paulo.menumaster.dto.request.CustomerRequestDto;
 import com.paulo.menumaster.dto.response.CustomerResponseDto;
+import com.paulo.menumaster.exception.custom.EntityExistingException;
 import com.paulo.menumaster.integration.ViaCepIntegration;
+import com.paulo.menumaster.model.City;
 import com.paulo.menumaster.model.State;
 import com.paulo.menumaster.repository.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +22,7 @@ public class CustomerService {
     private final NeighborhoodRepository neighborhoodRepository;
     private final StateRepository stateRepository;
     private final ViaCepIntegration viaCepIntegration;
+    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
 
     public CustomerService(CustomerRepository customerRepository, AddressRepository addressRepository,
                            CityRepository cityRepository, NeighborhoodRepository neighborhoodRepository,
@@ -35,7 +40,12 @@ public class CustomerService {
         var cepReturned = viaCepIntegration.searchCep(requestDto.address().cep());
         State stateReturned = createNewStateIfNotExist(normalizeTrim(cepReturned.estado()));
 
-
+        if(verifyIfTheStateHasTheCity(normalizeTrim(cepReturned.localidade()), stateReturned)){
+            log.info("The city: {} already belongs to the state: {}", cepReturned.localidade(), stateReturned.getName());
+        }else{
+            City newCity = new City(cepReturned.localidade(), stateReturned);
+            var citySaved = cityRepository.save(newCity);
+        }
 
     }
 
@@ -45,5 +55,9 @@ public class CustomerService {
             return stateRepository.save(newState);
         });
         return state;
+    }
+
+    private boolean verifyIfTheStateHasTheCity(String cityName, State state){
+        return cityRepository.existsByNameignoreCaseAndState(cityName, state);
     }
 }
